fc8ddaf02b7a5a70a6729a99ac93c145e8575bb6
Edgar <edgar_demin@mail.ru>
2021-04-02 15:05:56 +0300

    load

---
+++ js/price.js
@@ -0,0 +1,20 @@
+$('.ru_btn').on('click', function() {
+  $('.order_theory').text('7990 ₽');
+  $('.order_practice').text('9990 ₽');
+  $('.order_vip').text('24990 ₽');
+});
+$('.hr_btn').on('click', function() {
+  $('.order_theory').text('2990 ₴');
+  $('.order_practice').text('3690 ₴');
+  $('.order_vip').text('9290 ₴');
+});
+$('.usa_btn').on('click', function() {
+  $('.order_theory').text('109 $');
+  $('.order_practice').text('129 $');
+  $('.order_vip').text('329 $');
+});
+$('.eu_btn').on('click', function() {
+  $('.order_theory').text('89 €');
+  $('.order_practice').text('109 €');
+  $('.order_vip').text('279 €');
+});
---
+++ js/script.js
@@ -0,0 +1,108 @@
+///----------hamburger-menu-----------------------------///
+
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.top-menu'),
+  menuItem = document.querySelectorAll('.top-menu_item'),
+  hamburger = document.querySelector('.hamburger'),
+  wrap = document.querySelector('.top-menu__wrap');
+
+  hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('top-menu_active');
+      wrap.classList.toggle('top-menu__wrap_active');
+  });
+
+  menuItem.forEach(item => {
+      item.addEventListener('click', () => {
+          hamburger.classList.toggle('hamburger_active');
+          menu.classList.toggle('top-menu_active');
+          wrap.classList.toggle('top-menu__wrap_active');
+      })
+  })
+});
+
+///------------------menu-fixed---------------------///
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 500) {
+            $('.default-menu').addClass('default-menu_fixed');
+        } else {
+            $('.default-menu').removeClass('default-menu_fixed');
+        }
+  });
+});
+
+///--------------slaider-----------------///
+
+    $('.friends__wrapper').slick({
+        autoplay:true,
+        autoplaySpeed:1200,
+        arrows:false
+        /* prevArrow:'<button type="button" class="slick-prev"><img src="../img/chevron-left-solid.png"></button>',
+        nextArrow:'<button type="button" class="slick-prev"><img src="../img/chevron-right-solid.png"></button>' */
+    });
+
+
+///--------------slow-scroll-----------------///
+
+    $("a[href^='#']").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+});
+
+///--------------modal-window-----------------///
+
+$('.modal__close').on('click', function (){
+    $('.overlay, .modal__forms').fadeOut();
+  })
+  
+  $('.buy_btn').each(function(i) {
+    $(this).on('click', function() {
+      $('#buy .modal__pack').text('Пакет ' + $('.packs-descr__name').eq(i).text());
+      $('#buy .modal__descr').text($('.order').eq(i).text());
+      $('.overlay, #buy').fadeIn('slow');
+    })
+  });
+
+  $('.order__btn').each(function(i) {
+    $(this).on('click', function() {
+      $('#buy .modal__pack').text('Пакет ' + $('.order__name').eq(i).text());
+      $('#buy .modal__descr').text($('.order__newprice').eq(i).text());
+      $('.overlay, #buy').fadeIn('slow');
+    })
+  });
+
+///--------------animate-settings-----------------/// 
+
+new WOW().init();
+
+///--------------mask-phone-----------------/// 
+
+$('input[name=phone]').mask("+7 (999) 999-99-99");
+
+///--------------validate-----------------/// 
+
+function valideForms(form) {
+  $(form).validate({
+    rules: {
+      name: {
+        required: true,
+        minlength: 2
+      },
+      phone: "required"
+  },
+  messages: {
+    name: {
+      required: "Введите ваше имя",
+      minlength: jQuery.validator.format("Введите больше {0} символов")
+    },
+    phone: "неверный формат номера"
+  }
+  });
+}
+valideForms('#buy form');
+
+
+
